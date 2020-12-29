8b14a0d6b4da4eec4989d778a4320d21c1a031ef
NDPenaloza <ndpenaloza@gmail.com>
2020-12-28 16:55:55 -0800

    added sql

---
+++ employee-data.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee-data;
+
+CREATE DATABASE employee-data;
+
+USE employee-data;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id)
+);
