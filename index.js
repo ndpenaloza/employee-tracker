9c92c7c284819662daece8006279ac4327bd8e68
NDPenaloza <ndpenaloza@gmail.com>
2020-12-28 20:54:53 -0800

    added new js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const password = require("password");
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: password,
+    database: "employee-data"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
